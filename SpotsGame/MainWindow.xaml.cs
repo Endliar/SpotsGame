bcb496da69a357ba9270b96807c7f351578ea154
endliar <endliar@yandex.ru>
2024-01-15 22:55:14 +0700

    Рандомная генерация поля при запуске и перемещение плиток. Жуткий говнокод, конечно, но что поделать, спешка.

--- SpotsGame/MainWindow.xaml.cs
+++ SpotsGame/MainWindow.xaml.cs
@@ -1,30 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SpotsGame
 {
     public partial class MainWindow : Window
     {
+        private MixTiles _mixTiles = new MixTiles();
         public MainWindow()
         {
             InitializeComponent();
+            var shuffledState = _mixTiles.Shuffle(4);
+            InitializeTiles(shuffledState);
         }
 
         private void SpotsButton_click (object sender, RoutedEventArgs e)
         {
+            Button clickedButton = (Button)sender;
 
+            int row = Grid.GetRow(clickedButton);
+            int column = Grid.GetColumn(clickedButton);
+
+            if (IsMovable(row, column))
+            {
+                Button emptyButton = FindEmptyButton();
+                SwapButtons(clickedButton, emptyButton);
+            }
+        }
+
+        private void InitializeTiles(int[,] shuffledState)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    var button = (Button)GameGrid.Children.Cast<UIElement>().First(e => Grid.GetRow(e) == i && Grid.GetColumn(e) == j);
+                    button.Content = shuffledState[i, j] == 0 ? "" : shuffledState[i, j].ToString();
+                }
+            }
+        }
+
+        private bool IsMovable(int row, int column)
+        {
+            Button emptyButton = FindEmptyButton();
+            int emptyRow = Grid.GetRow(emptyButton);
+            int emptyCol = Grid.GetColumn(emptyButton);
+
+            return (row == emptyRow && Math.Abs(column - emptyCol) == 1) || (column == emptyCol && Math.Abs(row - emptyRow) == 1);
+        }
+
+        private void SwapButtons (Button clickedButton, Button emptyButton)
+        {
+            int clickedRow = Grid.GetRow(clickedButton);
+            int clickedColumn = Grid.GetColumn(clickedButton);
+            int emptyRow = Grid.GetRow(emptyButton);
+            int emptyColumn = Grid.GetColumn(emptyButton);
+
+            object clickedContent = clickedButton.Content;
+            clickedButton.Content = emptyButton.Content;
+            emptyButton.Content = clickedContent;
+
+            Grid.SetRow(clickedButton, emptyRow);
+            Grid.SetColumn(clickedButton, emptyColumn);
+            Grid.SetRow(emptyButton, clickedRow);
+            Grid.SetColumn(emptyButton, clickedColumn);
+
+            _mixTiles.SwapTiles(clickedRow, clickedColumn, emptyRow, emptyColumn);
+            InitializeTiles(_mixTiles.GetShuffledTiles());
+        }
+
+        private Button FindEmptyButton()
+        {
+            foreach (UIElement element in GameGrid.Children)
+            {
+                Button button = element as Button;
+                if (button != null && button.Content.ToString() == "")
+                {
+                    return button;
+                }
+            }
+            return null;
         }
     }
 }
---
+++ SpotsGame/MixTiles.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpotsGame
+{
+    public class MixTiles
+    {
+        private Random _random = new Random();
+        private int[,] _tiles;
+
+        public int[,] Shuffle(int size)
+        {
+            InitializeGame(size);
+            for (int moves = 0; moves < 1000; moves++)
+            {
+                MoveEmptyRandomly(size);
+            }
+
+            return (int[,])_tiles.Clone();
+        }
+
+        public void SwapTiles(int row1, int col1, int row2, int col2)
+        {
+            int temp = _tiles[row1, col1];
+            _tiles[row1, col1] = _tiles[row2, col2];
+            _tiles[row2, col2] = temp;
+        }
+
+        public void InitializeGame(int size)
+        {
+            _tiles = new int[size, size];
+            int count = 1;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    _tiles[i, j] = count++;
+                }
+            }
+            _tiles[size - 1, size - 1] = 0;
+        }
+
+        public int[,] GetShuffledTiles()
+        {
+            return (int[,])_tiles.Clone();
+        }
+
+        private void MoveEmptyRandomly(int size)
+        {
+            int emptyRow = 0, emptyCol = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (_tiles[i, j] == 0)
+                    {
+                        emptyRow = i;
+                        emptyCol = j;
+                    }
+                }
+            }
+
+            var validMoves = new List<(int, int)>();
+            if (emptyRow > 0) validMoves.Add((emptyRow - 1, emptyCol));
+            if (emptyRow < size - 1) validMoves.Add((emptyRow + 1, emptyCol));
+            if (emptyCol > 0) validMoves.Add((emptyRow, emptyCol - 1));
+            if (emptyCol < size - 1) validMoves.Add((emptyRow, emptyCol + 1));
+
+            var move = validMoves[_random.Next(validMoves.Count)];
+            SwapTiles(emptyRow, emptyCol, move.Item1, move.Item2);
+        }
+    }
+}
